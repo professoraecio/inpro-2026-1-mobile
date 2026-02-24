526799b77236b9709ba3bc175ba464cfea1cf0e4
Professor Aécio <professoraecio@gmail.com>
2026-02-23 21:08:55 -0300

    Finalizar projeto Seek Bar

--- ExemploSeekBar/app/src/main/java/com/professoraecio/exemploseekbar/MainActivity.java
+++ ExemploSeekBar/app/src/main/java/com/professoraecio/exemploseekbar/MainActivity.java
@@ -1,14 +1,39 @@
 package com.professoraecio.exemploseekbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView alturaTextView;
+    SeekBar alturaSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alturaTextView = findViewById(R.id.alturaTextView);
+        alturaSeekBar = findViewById(R.id.alturaSeekBar);
+
+        alturaSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                alturaTextView.setText("" + i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
