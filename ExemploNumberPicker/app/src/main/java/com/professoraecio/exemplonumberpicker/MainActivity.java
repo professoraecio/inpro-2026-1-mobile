7bf7a694149bc7d070fc3923ffc8ff82b7671c7e
Professor Aécio <professoraecio@gmail.com>
2026-02-26 19:22:04 -0300

    Finalizar projeto Number Picker

--- ExemploNumberPicker/app/src/main/java/com/professoraecio/exemplonumberpicker/MainActivity.java
+++ ExemploNumberPicker/app/src/main/java/com/professoraecio/exemplonumberpicker/MainActivity.java
@@ -1,14 +1,34 @@
 package com.professoraecio.exemplonumberpicker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView idadeTextView;
+    NumberPicker idadeNumberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idadeTextView = findViewById(R.id.idadeTextView);
+        idadeNumberPicker = findViewById(R.id.idadeNumberPicker);
+
+        idadeTextView.setText("Idade Selecionada: Nenhuma");
+
+        idadeNumberPicker.setMinValue(0);
+        idadeNumberPicker.setMaxValue(120);
+
+        idadeNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                idadeTextView.setText("Idade Selecionada: " + i);
+            }
+        });
+
     }
 }
