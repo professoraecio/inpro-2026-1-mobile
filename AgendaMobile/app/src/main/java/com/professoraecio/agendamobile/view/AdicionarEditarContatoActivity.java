edf109a66f58973d94471a3cdc873d227e9a7804
Professor Aécio <professoraecio@gmail.com>
2026-04-30 19:22:52 -0300

    finalizar layout

--- AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/AdicionarEditarContatoActivity.java
+++ AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/AdicionarEditarContatoActivity.java
@@ -3,14 +3,28 @@ package com.professoraecio.agendamobile.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.professoraecio.agendamobile.R;
 
 public class AdicionarEditarContatoActivity extends AppCompatActivity {
 
+    EditText nomeEditText;
+    EditText emailEditText;
+    EditText foneEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_editar_contato);
+
+        nomeEditText = findViewById(R.id.nomeEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        foneEditText = findViewById(R.id.foneEditText);
+    }
+
+    public void salvarButtonAction(View view){
+
     }
 }
--- AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/DetalhesContatoActivity.java
+++ AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/DetalhesContatoActivity.java
@@ -3,14 +3,23 @@ package com.professoraecio.agendamobile.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.professoraecio.agendamobile.R;
 
 public class DetalhesContatoActivity extends AppCompatActivity {
 
+    TextView nomeTextView;
+    TextView emailTextView;
+    TextView foneTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_contato);
+
+        nomeTextView = findViewById(R.id.nomeTextView);
+        emailTextView = findViewById(R.id.emailTextView);
+        foneTextView = findViewById(R.id.foneTextView);
     }
 }
