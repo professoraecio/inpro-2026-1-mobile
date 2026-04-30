fbfd97634621275827d10c41f70603e80a60086d
Professor Aécio <professoraecio@gmail.com>
2026-04-30 19:11:54 -0300

    layouts lista de contatos e editar excluir contato

---
+++ AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/DetalhesContatoActivity.java
@@ -0,0 +1,16 @@
+package com.professoraecio.agendamobile.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.professoraecio.agendamobile.R;
+
+public class DetalhesContatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_contato);
+    }
+}
---
+++ AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/ListaContatosActivity.java
@@ -0,0 +1,29 @@
+package com.professoraecio.agendamobile.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.professoraecio.agendamobile.R;
+
+public class ListaContatosActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_contatos);
+
+        listView = findViewById(R.id.listView);
+    }
+
+    public void adicionarContato(View view){
+        Intent intent = new Intent(this,
+                AdicionarEditarContatoActivity.class);
+        startActivity(intent);
+    }
+}
