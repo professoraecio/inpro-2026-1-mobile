713b34c517afa98bc53b1b7ed982ca5da9123ee0
Professor Aécio <professoraecio@gmail.com>
2026-05-04 20:59:44 -0300

    Finalizar classe contato api

--- AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/api/ContatoApi.java
+++ AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/api/ContatoApi.java
@@ -11,4 +11,56 @@ public class ContatoApi {
         String contatoJson = gson.toJson(contato);
         return contatoJson;
     }
+
+    public String inserir(Contato contato){
+        String contatoJson = this.toJson(contato);
+        HttpHelper httpHelper = new HttpHelper();
+        String response =
+                httpHelper.post(
+                        "/api.agenda/contato-dao/create",
+                        contatoJson
+                );
+        return response;
+    }
+
+    public String buscar(Contato contato){
+        HttpHelper httpHelper = new HttpHelper();
+        String response =
+                httpHelper.get(
+                        "/api.agenda/contato-dao/getId?id=" +
+                                contato.getId_contato()
+                );
+        return response;
+    }
+
+    public String todos(){
+        HttpHelper httpHelper = new HttpHelper();
+        String response =
+                httpHelper.get(
+                        "/api.agenda/contato-dao/getAll"
+                );
+        return response;
+    }
+
+    public String atualizar(Contato contato){
+        String contatoJson = this.toJson(contato);
+        HttpHelper httpHelper = new HttpHelper();
+        String response =
+                httpHelper.post(
+                        "/api.agenda/contato-dao/updateId",
+                        contatoJson
+                );
+        return response;
+    }
+
+    public String excluir(Contato contato){
+        String contatoJson = this.toJson(contato);
+        HttpHelper httpHelper = new HttpHelper();
+        String response =
+                httpHelper.post(
+                        "/api.agenda/contato-dao/deleteId",
+                        contatoJson
+                );
+        return response;
+    }
 }
