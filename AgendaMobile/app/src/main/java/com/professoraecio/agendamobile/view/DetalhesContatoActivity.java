5c36e6e8d1750fb2df373716c635dd4d60291ccf
Professor Aécio <professoraecio@gmail.com>
2026-05-04 21:29:36 -0300

    vincular métodos em detalhes activity

--- AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/DetalhesContatoActivity.java
+++ AgendaMobile/app/src/main/java/com/professoraecio/agendamobile/view/DetalhesContatoActivity.java
@@ -2,12 +2,17 @@ package com.professoraecio.agendamobile.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.professoraecio.agendamobile.R;
+import com.professoraecio.agendamobile.model.Contato;
 
 public class DetalhesContatoActivity extends AppCompatActivity {
+
+    Contato contato = new Contato();
 
     TextView nomeTextView;
     TextView emailTextView;
@@ -19,7 +24,24 @@ public class DetalhesContatoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalhes_contato);
 
         nomeTextView = findViewById(R.id.nomeTextView);
-        emailTextView = findViewById(R.id.emailTextView);
         foneTextView = findViewById(R.id.foneTextView);
+        emailTextView = findViewById(R.id.emailTextView);
+
+        Intent intent = getIntent();
+        if(intent.hasExtra("contato")){
+            contato = (Contato) intent.getSerializableExtra("contato");
+            nomeTextView.setText(contato.getNome());
+            foneTextView.setText(contato.getFone());
+            emailTextView.setText(contato.getEmail());
+        }
     }
+
+    public void editar(View view){
+
+    }
+
+    public void excluir(View view){
+
+    }
+
 }
